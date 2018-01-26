33135b3d5ac8e4bceef6a42cc70a43cf6bad3511
joepuzzo <jgpuzzo2@gmail.com>
2018-01-25 19:32:36 -0500

    fixed issues with images on mobile and added tool tip dropdown

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip';
 
 import Scroll from './Scroll';
 
-const NavItem = ({ path, text }) => (
+const NavItem = ({ path, text, ...rest }) => (
   <li className="navbar-item">
-    <Link className="navbar-link" to={path}>{text}</Link>
+    <Link className="navbar-link" to={path} {...rest}>{text}</Link>
   </li>
 );
+
+const PopoverItem = ({ path, text, ...rest }) => (
+  <li className="popover-item">
+    <Link className="popover-link" to={path} {...rest}>{text}</Link>
+  </li>
+);
+
+class PopOver extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.close.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.close.bind(this));
+  }
+
+  toggle(e) {
+    e.preventDefault();
+    this.setState( ( prevState ) => ({
+      open: !prevState.open
+    }));
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+  }
+
+  close() {
+    this.setState({ open: false });
+    console.log("HERE");
+  }
+
+  render() {
+    return (
+      <li className="navbar-item">
+        <a
+          onClick={(e)=>{ this.toggle(e) }}
+          className="navbar-link"
+          href="#"
+          data-popover="#codeNavPopover">
+          {this.props.text}
+        </a>
+        <div id="codeNavPopover" className={`popover ${ this.state.open ? 'open' : ''}`} style={this.props.style}>
+          {this.props.children}
+        </div>
+      </li>
+    );
+  }
+}
 
 const Nav = () => (
   <div>
@@ -19,8 +75,12 @@ const Nav = () => (
             <ul className="navbar-list">
               <NavItem path="/home" text="Home" />
               <NavItem path="/about" text="About" />
-              <NavItem path="/statemachine" text="IPV6 State Machine" />
-              <NavItem path="#home" text="Bar" />
+              <PopOver text="Projects" style={{ left: '-30%' }}>
+                <ul className="popover-list" style={{right: 20}}>
+                  <PopoverItem path="/statemachine" text="StateMachine" />
+                  <PopoverItem path="/statemachine" text="StateMachine" />
+                </ul>
+              </PopOver>
             </ul>
           </div>
         </nav>
--- src/components/StateMachine.js
+++ src/components/StateMachine.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Fade } from 'react-reveal';
 
 const StateMachine = () => (
   <div className="bg-grey">
     <section className="section container">
       <h2 className="text-center">IPV6 State Machine</h2>
       <div className="row mb-3">
-        <div className="twelve columns text-center">
-          <img alt="v6 state machine" src="/assets/images/v6StateMachine.png" />
-        </div>
+        <Fade delay={150} duration={3000} className="col text-center">
+          <img className="state-machine" alt="v6 state machine" src="/assets/images/v6StateMachine.png" />
+        </Fade>
       </div>
+      {/* <div className="row mb-3">
+        <div className="twelve columns text-center">
+          <img className="state-machine" alt="v6 state machine" src="/assets/images/v6StateMachine.png" />
+        </div>
+      </div> */}
     </section>
   </div>
 );
